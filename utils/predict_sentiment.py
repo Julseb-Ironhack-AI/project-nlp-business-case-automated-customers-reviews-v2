d2e53b1a4c06aa056c401ad25b040461d921a206
Julien <julien.sebag@me.com>
2025-06-13 12:26:12 +0200

    Fix predict_sentiment

--- utils/predict_sentiment.py
+++ utils/predict_sentiment.py
@@ -1,8 +1,28 @@
-#Function to predict sentiment
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+# Load tokenizer and model
+model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+# Set model to evaluation mode
+model.eval()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Define sentiment labels
+labels = ["Negative", "Neutral", "Positive"]
+
+
+# Function to predict sentiment
 def classify_sentiment(text):
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512).to(device)
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512).to(
+        device
+    )
     with torch.no_grad():
         outputs = model(**inputs)
         probs = torch.nn.functional.softmax(outputs.logits, dim=1)
         predicted_class = torch.argmax(probs).item()
-    return labels[predicted_class]  
+    return labels[predicted_class]
